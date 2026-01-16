6c3ff3573fb884a312569b8106572a0942e56e00
Shivam Shukla <shivamshukla@vasyerp.com>
2026-01-16 12:16:02 +0530

    created bank management system also added executorservice and read write lock

---
+++ src/Day_8/BankOperations.java
@@ -0,0 +1,15 @@
+package Day_8;
+
+public interface BankOperations {
+    void deposit(double amount) throws InvalidAmountException;
+    void withdraw(double amount) throws InsufficientBalanceException;
+
+    default boolean isValidAmount(double amount) {
+        return amount > 0;
+    }
+
+    static void showRules() {
+        System.out.println("Minimum balance must be maintained.");
+    }
+}
+
---
+++ src/Day_8/Customer.java
@@ -0,0 +1,59 @@
+package Day_8;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private int customerId;
+    private String name;
+    // Dynamic Method Dispatch: Reference of Parent class Account and objects of child Current and Saving Account
+    private ArrayList<Account> accounts = new ArrayList<>();
+
+    public Customer(int id, String name) {
+        this.customerId = id;
+        this.name = name;
+    }
+    public int getCustomerId(){
+        return this.customerId;
+    }
+
+    public void addAccount(Account acc) {
+        accounts.add(acc);
+    }
+
+    public Account getAccount(int accNo) throws AccountNotFoundException {
+        for (Account a : accounts) {
+            if (a.getAccountNumber() == accNo)
+                return a;
+        }
+        throw new AccountNotFoundException("Account not found!");
+    }
+
+    public void displayAccounts() {
+        if(accounts.isEmpty()) {System.out.println(
+                "No Accounts!"
+        );
+            return;}
+        for (Account a : accounts)
+            a.displayAccount();
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", name='" + name + '\'' +
+                ", accounts count=" + accounts.size() +
+                '}';
+    }
+
+    public void deductFee(int amount) throws InsufficientBalanceException {
+        for (Account acc : accounts) {
+            acc.withdraw(amount);
+        }
+    }
+    public void addReward(int amount) throws InvalidAmountException {
+        for (Account acc : accounts) {
+            acc.deposit(amount);
+        }
+    }
+}
